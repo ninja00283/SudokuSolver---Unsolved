ce7e130ca62d7bff352e86bbd51ce4faab6905d1
jan bos <max.bos217@gmail.com>
2019-03-21 14:43:18 +0100

    3

--- SudokuSolver/Logics/Solver.cs
+++ SudokuSolver/Logics/Solver.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
-using System.Diagnostics;
 
 namespace SudokuSolver.Logics
 {
@@ -15,37 +14,78 @@ namespace SudokuSolver.Logics
         /// </summary>
         /// <param name="sudoku"> The unsolved sudoku</param>
         /// <returns></returns>
-        public int[][] Solve(int[][] sudoku)
-        {
-            while (true)
-            {
-                List<int[][]> sudokuList = SolveSudoku(sudoku);
-
-                if (sudokuList.Count == 1) {
-                    break;
-                }
-            }
-            return sudoku;
-        }
-
-        public List<int[][]> SolveSudoku(int[][] sudoku) {
+        public int[][] Solve(int[][] sudoku) {
             List<int[][]> sudokuList = new List<int[][]>();
+            bool firstRun = true;
+            sudokuList.Add(sudoku);
+
+            List<int[][]> sudokuListTemp = new List<int[][]>();
             for (int x = 0; x < sudoku.Length; x++)
             {
                 for (int y = 0; y < sudoku.Length; y++)
                 {
-                    if (!CheckIfEmpty(sudoku, x, y))
+                    foreach (var item in sudokuList)
                     {
-                        List<int> exclude = CheckX(sudoku, y);
-                        exclude = AddList(exclude, CheckY(sudoku, x));
-                        exclude = AddList(exclude, CheckGrid(sudoku, 3, 3, x, y));
-                        List<int> toAdd = RemoveNumbers(exclude, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
-                        foreach (var item in toAdd)
+                        if (firstRun)
                         {
-                            sudoku[y][x] = item;
-                            sudokuList.Add(sudoku);
+                            sudokuList = SolveSudoku(item, x, y);
+                            if (sudokuList.Count > 0) {
+                                firstRun = false;
+                            }
+                            else
+                            {
+                                sudokuList.Add(sudoku);
+                            }
+                        }
+                        else
+                        {
+                            sudokuList.AddRange(SolveSudoku(item, x, y));
+                            foreach (var spot in sudokuList)
+                            {
+                                Debug.WriteLine("--");
+                                foreach (var spotX in spot)
+                                {
+                                    foreach (var spotY in spotX)
+                                    {
+                                        Debug.Write(spotY.ToString());
+                                    }
+                                    Debug.WriteLine("");
+                                }
+                            }
                         }
                     }
+                }
+            }
+
+            sudoku = sudokuList.Last() ;
+            return sudoku;
+        }
+
+        public List<int[][]> SolveSudoku(int[][] sudoku,int x,int y) {
+            List<int[][]> sudokuList = new List<int[][]>();
+            
+            if (!CheckIfEmpty(sudoku, x, y))
+            {
+                List<int> exclude = CheckX(sudoku, y);
+                exclude = AddList(exclude, CheckY(sudoku, x));
+                exclude.AddRange(CheckGrid(sudoku, 3, 3, x, y));
+                List<int> toAdd = RemoveNumbers(exclude, new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+                foreach (var item in toAdd)
+                {
+                    sudokuList.Add(sudoku);
+                }
+                for (int i = 0; i < toAdd.Count; i++)
+                {
+                    sudokuList[i][y][x] = toAdd[i];
+                    foreach (var spotX in sudokuList[i])
+                    {
+                        foreach (var spotY in spotX)
+                        {
+                            Debug.Write(spotY.ToString());
+                        }
+                        Debug.WriteLine("");
+                    }
+
                 }
             }
             return sudokuList;
@@ -61,7 +101,6 @@ namespace SudokuSolver.Logics
                 }
                 catch (Exception e)
                 {
-                    Debug.WriteLine(e.ToString());
                 }
             }
             return toExcludeFrom;
@@ -77,6 +116,7 @@ namespace SudokuSolver.Logics
         public bool CheckIfEmpty(int[][] sudoku, int x, int y) {
             return (sudoku[y][x] != 0);
         }
+
         /// <summary>
         /// Take a int list(list1) and add it to the end of another list(list0).
         /// </summary>
@@ -84,6 +124,21 @@ namespace SudokuSolver.Logics
         /// <param name="list1"> list to add </param>
         /// <returns></returns>
         public List<int> AddList(List<int> list0, List<int> list1) {
+            foreach (var item in list1)
+            {
+                list0.Add(item);
+            }
+            return list0;
+        }
+
+        /// <summary>
+        /// Take a int list(list1) and add it to the end of another list(list0).
+        /// </summary>
+        /// <param name="list0"> list to add to</param>
+        /// <param name="list1"> list to add </param>
+        /// <returns></returns>
+        public List<int[][]> AddList(List<int[][]> list0, List<int[][]> list1)
+        {
             foreach (var item in list1)
             {
                 list0.Add(item);
@@ -144,9 +199,9 @@ namespace SudokuSolver.Logics
                 for (int x = 0; x < width; x++)
                 {
                 
-                    if (CheckIfEmpty(sudoku, x + xPos % width, y + yPos % height))
+                    if (CheckIfEmpty(sudoku, xPos - xPos % width + x, yPos - yPos % height + y))
                     {
-                        exclude.Add(sudoku[y + yPos % height][x + xPos % width]);
+                        exclude.Add(sudoku[yPos - yPos % height + y][xPos - xPos % width + x]);
                     }
                 }
             }
