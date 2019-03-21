cb149a77908d9abdf20ae7ffdbe84e6ae11a7aec
jan bos <max.bos217@gmail.com>
2019-03-21 10:22:36 +0100

    comments

--- SudokuSolver/Logics/Solver.cs
+++ SudokuSolver/Logics/Solver.cs
@@ -4,26 +4,52 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
+using System.Diagnostics;
 
 namespace SudokuSolver.Logics
 {
     public class Solver
     {
+        /// <summary>
+        /// Take a sudoku and solve it.
+        /// </summary>
+        /// <param name="sudoku"> The unsolved sudoku</param>
+        /// <returns></returns>
         public int[][] Solve(int[][] sudoku)
         {
             for (int x = 0; x < sudoku.Length; x++)
             {
                 for (int y = 0; y < sudoku.Length; y++)
                 {
+                    if (CheckIfEmpty(sudoku, x, y)) {
+                        List<int> exclude = CheckX(sudoku, y);
+                        exclude = AddList(exclude, CheckY(sudoku, x));
+                        exclude = AddList(exclude, CheckGrid(sudoku, 3, 3, x, y));
 
-                    List<int> exclude = CheckX(sudoku, y);
-                    exclude = AddList(exclude, CheckY(sudoku,x));
-                    exclude = AddList(exclude, CheckGrid(sudoku,3,3,x,y));
+                    }
                 }
             }
             return sudoku;
         }
 
+
+
+        /// <summary>
+        /// Check if the given position int the given sudoku is empty.
+        /// </summary>
+        /// <param name="sudoku"> The sudoku to check </param>
+        /// <param name="x"> The x position to check </param>
+        /// <param name="y"> The y position to check </param>
+        /// <returns></returns>
+        public bool CheckIfEmpty(int[][] sudoku, int x, int y) {
+            return (sudoku[y][x] != 0);
+        }
+        /// <summary>
+        /// Take a int list(list1) and add it to the end of another list(list0).
+        /// </summary>
+        /// <param name="list0"> list to add to</param>
+        /// <param name="list1"> list to add </param>
+        /// <returns></returns>
         public List<int> AddList(List<int> list0, List<int> list1) {
             foreach (var item in list1)
             {
@@ -32,24 +58,36 @@ namespace SudokuSolver.Logics
             return list0;
         }
 
+        /// <summary>
+        /// Check every Number in the x coordinate on the given y coordinate 
+        /// </summary>
+        /// <param name="sudoku"> The sudoku to check </param>
+        /// <param name="y"> The y coordinate of the row to check</param>
+        /// <returns></returns>
         public List<int> CheckX(int[][] sudoku, int y)
         {
-            List<int> exclude = new List<int>(); 
+            List<int> exclude = new List<int>();
             for (int x = 0; x < sudoku.Length; x++)
             {
-                if (sudoku[y][x] != 0) {
+                if (CheckIfEmpty(sudoku, x, y)) {
                     exclude.Add(sudoku[y][x]);
                 }
             }
             return exclude;
         }
 
+        /// <summary>
+        /// Check every number in the y coordinate on the given x coordinate
+        /// </summary>
+        /// <param name="sudoku"> The sudoku to check </param>
+        /// <param name="x"> The x coordinate of the collumn to check </param>
+        /// <returns></returns>
         public List<int> CheckY(int[][] sudoku, int x)
         {
             List<int> exclude = new List<int>();
             for (int y = 0; y < sudoku.Length; y++)
             {
-                if (sudoku[y][x] != 0)
+                if (CheckIfEmpty(sudoku, x, y))
                 {
                     exclude.Add(sudoku[y][x]);
                 }
@@ -57,13 +95,23 @@ namespace SudokuSolver.Logics
             return exclude;
         }
 
+        /// <summary>
+        /// Check every number of the appropriate grid of the given position of the given height and width
+        /// </summary>
+        /// <param name="sudoku"> The sudoku to check </param>
+        /// <param name="width"> The width of the grid to check </param>
+        /// <param name="height"> The height of the grid to check </param>
+        /// <param name="xPos"> The x position of the current grid </param>
+        /// <param name="yPos"> The y position of the current grid </param>
+        /// <returns></returns>
         public List<int> CheckGrid(int[][] sudoku, int width,int height,int xPos,int yPos) {
             List<int> exclude = new List<int>();
-            for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
             {
-                for (int y = 0; y < height; y++)
+                for (int x = 0; x < width; x++)
                 {
-                    if (sudoku[yPos][xPos] != 0)
+                
+                    if (CheckIfEmpty(sudoku, x + xPos % width, y + yPos % height))
                     {
                         exclude.Add(sudoku[y + yPos % height][x + xPos % width]);
                     }
